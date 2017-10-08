5e88a9700765971786de421d83121d1ced9e7ac0
grace-y728 <grace.nq.yu98@gmail.com>
2017-10-07 18:39:02 -0700

    Add menu bar UI

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//js
+$( document ).ready(function() {
+
+  //initialize
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+
+  //display menu
+  $(".hamburger").click(function () {
+    $( ".menu" ).slideToggle( "slow", function() {
+      $( ".hamburger" ).hide();
+      $( ".cross" ).show();
+    });
+  });
+
+  //close menu
+  $(".cross").click(function (){
+    $(".menu").slideToggle("slow", function(){
+      $(".hamburger").show();
+      $(".cross").hide();
+    })
+
+  });
+
+});
